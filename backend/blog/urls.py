f7cd48f3e1cbe360d5285b48f62d3fcdc7e223ea
nika-nii <farakshin.nikita@gmail.com>
2020-11-21 01:36:07 +0300

    Отдача статических данных

--- backend/blog/urls.py
+++ backend/blog/urls.py
@@ -13,9 +13,12 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from api.views import static_post, static_post_list
 from django.contrib import admin
 from django.urls import path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/posts', static_post_list),
+    path('api/post', static_post)
 ]
