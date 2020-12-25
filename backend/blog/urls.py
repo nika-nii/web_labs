f356a35cc6d4ea5582baf9d52c653f1645f2cb91
nika-nii <farakshin.nikita@gmail.com>
2020-12-25 03:33:57 +0300

    Добавлена документация swagger

--- backend/blog/urls.py
+++ backend/blog/urls.py
@@ -1,13 +1,33 @@
+from django.conf.urls import url
 from django.contrib import admin
 from django.urls import path
-from rest_framework import routers
+from rest_framework import routers, permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
 
 from api.views import PostViewSet
+
+
+schema_view = get_schema_view(
+   openapi.Info(
+      title="Snippets API",
+      default_version='v1',
+      description="Test description",
+      terms_of_service="https://www.google.com/policies/terms/",
+      contact=openapi.Contact(email="contact@snippets.local"),
+      license=openapi.License(name="BSD License"),
+   ),
+   public=True,
+   permission_classes=[permissions.AllowAny],
+)
 
 router = routers.SimpleRouter()
 router.register(r'posts', PostViewSet)
 
 urlpatterns = [
+    url(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    url(r'^swagger/$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    url(r'^redoc/$', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
     path("admin/", admin.site.urls),
 ]
 
