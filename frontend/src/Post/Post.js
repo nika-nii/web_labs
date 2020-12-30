008c97f6ee4e82fae7ba879981f8f98f35a7943f
nika-nii <farakshin.nikita@gmail.com>
2020-12-30 09:19:12 +0300

    Update Post.js

--- frontend/src/Post/Post.js
+++ frontend/src/Post/Post.js
@@ -1,11 +1,46 @@
-import React, {useEffect} from 'react'
+import React from 'react'
 import './Post.css'
+import ApiService from "../Services/ApiService";
 
 class Post extends React.Component {
-    render() {
-        const post = this.props.post
 
-        const content = post.content.map((piece) => {
+    apiservice = new ApiService();
+
+    state = {
+        user: null,
+        title: null,
+        text: null,
+        header_image: null,
+        creation_time: null,
+        content: null,
+    }
+
+    componentDidMount() {
+        this.apiservice
+            .getPost(this.props.id)
+            .then((data) => {
+                this.setState({
+                    user: data.user,
+                    title: data.title,
+                    text: data.text,
+                    header_image: data.header_image,
+                    creation_time: data.creation_time,
+                })
+            })
+    }
+
+    renderItems(items) {
+        return items.map((item) => {
+            return (
+                <span key={item.id}>{item.name} </span>
+            )
+        })
+    }
+
+    render() {
+        const {user, title, text, header_image, creation_time, content} = this.state
+
+        const rendered_content = content.map((piece) => {
                 if (piece.image_data) {
                     return <img className="mx-auto d-block rounded post-img m-3 w-100" src={piece.image_data}
                                 alt={piece.description}/>
@@ -32,41 +67,17 @@ class Post extends React.Component {
             }
         )
 
-        // Для нумерации строк в коде. TODO: заменить эту дичь на какую-нибудь библиотеку
-        useEffect(() => {
-            const script = document.createElement('script');
-            script.src = "(function () {\n" +
-                "            var pre = document.getElementsByTagName('pre'),\n" +
-                "                pl = pre.length;\n" +
-                "            for (var i = 0; i < pl; i++) {\n" +
-                "                pre[i].innerHTML = '<span class=\"line-number\"></span>' + pre[i].innerHTML + '<span class=\"cl\"></span>';\n" +
-                "                var num = pre[i].innerHTML.split(/\\n/).length;\n" +
-                "                for (var j = 0; j < num; j++) {\n" +
-                "                    var line_num = pre[i].getElementsByTagName('span')[0];\n" +
-                "                    line_num.innerHTML += '<span>' + (j + 1) + '</span>';\n" +
-                "                }\n" +
-                "            }\n" +
-                "        })();";
-            script.async = true;
-
-            document.body.appendChild(script);
-
-            return () => {
-                document.body.removeChild(script);
-            }
-        }, []);
-
         return (
             <div className="container py-3">
                 <div className="row justify-content-center">
                     <div className="col-12 col-lg-8 p-1">
                         <div className="card">
-                            <img className="post-img-top card-img-top" src={post.header_image}
+                            <img className="post-img-top card-img-top" src={header_image}
                                  alt="Картинка у поста"/>
                             <div className="card-body">
-                                <h5 className="card-title">{post.title}</h5>
-                                <h6 className="card-subtitle mb-2 text-muted">{post.date}</h6>
-                                {content}
+                                <h5 className="card-title">{title}</h5>
+                                <h6 className="card-subtitle mb-2 text-muted">{creation_time}</h6>
+                                {rendered_content}
                             </div>
                         </div>
                     </div>
